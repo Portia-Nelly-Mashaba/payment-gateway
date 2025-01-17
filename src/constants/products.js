6f9bca0f61c76cac2282d8f56878526803e7ae1d
Portia-Nelly-Mashaba <pnpnelly@gmail.com>
2025-01-17 19:44:53 +0200

    first commit

---
+++ src/constants/products.js
@@ -0,0 +1,50 @@
+const products = [
+    {
+        id: 1,
+        name: 'Her Perfume',
+        price: '102.99',
+        like: true,
+        img: require('../assets/perfume.png'),
+        about: 'A captivating fragrance that leaves a lasting impression.'
+    },
+    {
+        id: 2,
+        name: 'Puff Powder',
+        price: '25.99',
+        like: false,
+        img: require('../assets/puff_powder.png'),
+        about: 'A lightweight powder that gives your skin a flawless finish.'
+    },
+    {
+        id: 3,
+        name: 'Make-up Brushes',
+        price: '45.99',
+        like: true,
+        img: require('../assets/makeup_brushes.png'),
+        about: 'A set of high-quality brushes for perfect makeup application.'
+    },
+    {
+        id: 4,
+        name: 'Lipstick',
+        price: '15.99',
+        like: true,
+        img: require('../assets/lipstick.png'),
+        about: 'A long-lasting lipstick that provides rich color and hydration.'
+    },
+    {
+        id: 5,
+        name: 'Nail Polish',
+        price: '10.99',
+        like: false,
+        img: require('../assets/nail_polish.png'),
+        about: 'A vibrant nail polish that adds a pop of color to your nails.'
+    },
+    {
+        id: 6,
+        name: 'Face Serum',
+        price: '55.99',
+        like: true,
+        img: require('../assets/face_serum.png'),
+        about: 'A rejuvenating serum that nourishes and revitalizes your skin.'
+    }
+];
